d4c49a022be6598f916c71fb2a6f985c213610dd
zandy404 <zandy.lin@gmail.com>
2016-07-15 11:52:39 -0400

    Picasso

---
+++ app/src/main/java/com/mac/training/applicationapp/NetworkReceiver.java
@@ -0,0 +1,17 @@
+package com.mac.training.applicationapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class NetworkReceiver extends BroadcastReceiver {
+    public NetworkReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
